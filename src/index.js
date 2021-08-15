1c4b9e7d411221eafe9ef9c7277ca2c376ece1f4
divakotova <divakova.dasha@yandx.by>
2021-08-15 14:13:35 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable (num) {
+     let i = '', rank = ['','thousand','million','billion','trillion','quadrillion','quintillion'];
+  let numArray = num.toString().split(/(?=(?:\d{3})+$)/, 7);  
+    if (numArray == '' || numArray == '0') return 'zero'; 
+      if (numArray[0].length == 1) numArray[0] = '00'+ numArray[0];
+    if (numArray[0].length == 2) numArray[0] = '0'+ numArray[0];
+    for (var j = (numArray.length - 1); j >= 0; j--) { 
+      if (numArray[j] != '000') {
+        i = t(numArray[j]) + (rank[numArray.length - 1 - j] = 0 ?  '' : ' ' + rank[numArray.length - 1 - j]) + i;
+      }
+    }
+    function t(numArray) {
+      let numTitle = [
+  ['',' one',' two',' three',' four',' five',' six',' seven',' eight',' nine'],
+  [' ten',' eleven',' twelve',' thirteen',' fourteen',' fifteen',' sixteen',' seventeen',' eighteen',' nineteen'],
+  ['','',' twenty',' thirty',' forty',' fifty',' sixty',' seventy',' eighty',' ninety'],
+  [' hundred'],
+  ];
+      return (numArray[0] != 0 ? numTitle[0][numArray[0]] + numTitle[3] : '') + (numArray[1] == 1 ? numTitle[1][numArray[2]] : numTitle[2][numArray[1]] + numTitle[0][numArray[2]]);
+    }
+    return i.trim();
+  }
+  var toReadable = require('./index.js')
+  console.log(toReadable(105))
